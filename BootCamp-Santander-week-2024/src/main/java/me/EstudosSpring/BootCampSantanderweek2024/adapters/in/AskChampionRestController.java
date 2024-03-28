9685c7a44430926820e3b491ff30fdf3ae34a6b9
TheuZCoder <matheusrdsilva1@gmail.com>
2024-03-28 08:49:37 -0300

    Fix: a aplicação só funciona normalmente no INTELLIJ pelo vscode tem um bug que não descobri ainda

--- BootCamp-Santander-week-2024/src/main/java/me/EstudosSpring/BootCampSantanderweek2024/adapters/in/AskChampionRestController.java
+++ BootCamp-Santander-week-2024/src/main/java/me/EstudosSpring/BootCampSantanderweek2024/adapters/in/AskChampionRestController.java
@@ -1,28 +1,19 @@
 package me.EstudosSpring.BootCampSantanderweek2024.adapters.in;
 
-
-import me.EstudosSpring.BootCampSantanderweek2024.application.AskChampionsUseCase;
-
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import io.swagger.v3.oas.annotations.tags.Tag;
-
-
-
+import me.EstudosSpring.BootCampSantanderweek2024.application.AskChampionsUseCase;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Campeões", description = "Endpoints do domínio de Campeões do LOL.")
 @RestController
 @RequestMapping("/champions")
 public record AskChampionRestController(AskChampionsUseCase useCase) {
+
     public record AskChampionRequest(String question) { }
     public record AskChampionResponse(String answer) { }
-    
-    
-    @PostMapping("/{championsId}/ask")
+
+    @CrossOrigin
+    @PostMapping("/{championId}/ask")
     public AskChampionResponse askChampion(@PathVariable Long championId, @RequestBody AskChampionRequest request) {
         String answer = useCase.askChampion(championId, request.question());
         return new AskChampionResponse(answer);
